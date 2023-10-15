20e959bf4312f9ac3e08299a253d192acdac98ed
NabilKhattab37 <nabilkhattab17@yahoo.com>
2023-10-16 00:23:41 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import './globals.css';
 
 const darkTheme = createTheme({
     palette: {
